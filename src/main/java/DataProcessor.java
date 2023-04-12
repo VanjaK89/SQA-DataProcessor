f9a2bcfc3035060a9e37c35370b3a49c4e87ef73
vanja <kavgic.vanja@gmail.com>
2023-04-12 08:14:54 +0200

    Initial commit

---
+++ src/main/java/DataProcessor.java
@@ -0,0 +1,60 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class DataProcessor {
+    private final HashMap<String, Duration> result = new HashMap<>();
+    private final Holidays holidays;
+    public DataProcessor(Holidays holidays){
+        this.holidays = holidays;
+
+    }
+
+    HashMap<String, Duration> process(InputStream data) throws ParseException {
+        Scanner scanner = new Scanner(data);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+        String previousActivity = null;
+        Date previousDate = null;
+        while (scanner.hasNext()) {
+            String date = scanner.next("\\d+-\\d+-\\d+");
+            String time = scanner.next("\\d+:\\d+");
+            Date dateTime = dateFormat.parse(date + " " + time);
+            String activity = scanner.nextLine().trim();
+            sumDuration(previousActivity, previousDate, dateTime);
+            if ("*".equals(activity)) {
+                previousDate = null;
+            } else {
+                previousDate = dateTime;
+                previousActivity = activity;
+            }
+        }
+
+        return result;
+    }
+
+    private void sumDuration(String activity, Date previous, Date current) {
+        if (previous != null) {
+            Duration duration = calcDuration(previous, current);
+            if (!result.containsKey(activity)) {
+                result.put(activity, duration);
+            } else {
+                result.merge(activity, duration, Duration::plus);
+            }
+        }
+    }
+
+    private Duration calcDuration(Date previous, Date current) {
+        Duration duration = Duration.between(previous.toInstant(), current.toInstant());
+        if (holidays.isHoliday(previous)) {
+            return duration.multipliedBy(2);
+        }
+        return duration;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.text.ParseException;
+import java.time.Duration;
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) throws URISyntaxException, FileNotFoundException, ParseException {
+        URL pathURL = Main.class.getResource("data.txt");
+        assert pathURL != null;
+        File data = new File(pathURL.toURI());
+        FileInputStream dataStream = new FileInputStream(data);
+        DataProcessor dataProcessor = new DataProcessor(new Holidays());
+        HashMap<String, Duration> result = dataProcessor.process(dataStream);
+
+        for (String activity : result.keySet()) {
+            System.out.printf("Activity %s took %d minutes.\n", activity, result.get(activity).toMinutes());
+        }
+
+    }
+}
